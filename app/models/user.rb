bd3013d1fa2dc8934686606ed40456cc0e04e3cb
camilo palacios <camilop7@icloud.com>
2023-11-23 10:52:57 +0000

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :date_of_birth, presence: true
+  validate :date_of_birth_cannot_be_in_the_future
+
+  def date_of_birth_cannot_be_in_the_future
+    if date_of_birth.present? && date_of_birth > Date.today
+      errors.add(:date_of_birth, "can't be in the future")
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  RSpec.describe User, type: :model do
+    it "is valid with valid attributes" do
+      user = User.new(
+        name: "John Doe",
+        date_of_birth: "1990-01-01",
+      )
+
+      expect(user).to be_valid
+
+  end
+
+  it "should be invalid with a date of birth in the future" do
+    user = User.new(
+      name: "John Doe",
+      date_of_birth: Time.now + 1.day,
+    )
+
+    expect(user).not_to be_valid
+
+  end
 end
